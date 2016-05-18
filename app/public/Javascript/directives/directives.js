e044a6609519a999f564453f0d02218e577b1da4
LeoQuint <leonardquintal@gmail.com>
2016-05-18 14:27:46 -0400

    Last update

--- app/public/Javascript/directives/directives.js
+++ app/public/Javascript/directives/directives.js
@@ -1,1 +1,18 @@
-angular.module('psdnetApp.directives.form', []);
+//testing directive
+
+angular.module('psdnetApp.directives.form', [])
+	.directive('testingCard', function(){
+		return {
+			restrict: 'E',
+			templateUrl: "views/About/contact.html",
+			link: function(scope, element, attrs){
+				console.log(arguments);
+				element.on("click", function(){
+					alert('clicked');
+				});
+			},
+			controller: function($scope){
+				console.log($scope.ontarioColleges);
+			}
+		};
+	});
